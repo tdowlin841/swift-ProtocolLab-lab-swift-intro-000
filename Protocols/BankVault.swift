c81edfa8f850a7419981f385e5f610e5aa6a10b5
Terrence <Terrence@Terrences-iMac.local>
2017-02-15 12:58:13 -0500

    TheBank

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -12,14 +12,64 @@ class BankVault {
     let name: String
     let address: String
     var amount: Double = 0.0
-    
+
     init(name: String, address: String) {
         self.name = name
         self.address = address
     }
-    
 }
 
+protocol ProvideAccess {
+    func allowEntryWithPassword(password : [Int]) -> Bool
+    }
+    
+extension BankVault: ProvideAccess {
+    func allowEntryWithPassword(password: [Int]) -> Bool {
+    
+    let otherNumbers = [password[0], password[2], password[4], password [6], password[8] ]
+    let evenNumbers = [2, 4, 6, 8,]
+    
+    if password == [] {
+        if password.count > 10 {
+            return false
+        }
+    } else {
+        for number in otherNumbers {
+            for digit in evenNumbers {
+                if number == digit {
+                    return true
+                    }
+                }
+            }
+        }
+    return false
+    }
+}
+
+/*(1) - In the BankVault.swift file, create a protocol named ProvideAccess.
+ 
+ This protocol should require any class/struct that conforms to it to 
+ 
+ implement a function called allowEntryWithPassword which takes in one argument called password of type [Int] (an array of Ints). The function should return a Bool.
+
+
+(2) - In the BankVault.swift file, below where you created the ProvideAccess protocol - create an extension on the BankVault class. 
+ 
+ In that extension, it should conform to the ProvideAccess protocol. 
+ 
+ In your implementation of the allowEntryWithPassword(_:) function, you should adhere to the following rules:
+ If the person calling on this function doesn't enter in any digits (empty array), then we should return false.
+ 
+If the total numbers of digits entered (items in the array) exceed 10, then we should return false.
+ 
+In order to allow access (return true), every other digit (starting with the first digit) must be even.
+ 
+ The first digit entered is the first element in the array.
+ 
+
+There are tests in the BankVaultTests.swift file. Press command + u to run these tests to make sure you implemented the allowEntryWithPassword(_:) function correctly.
+
+ */
 
 
 
--- Protocols/Ships.swift
+++ Protocols/Ships.swift
@@ -6,8 +6,58 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
+enum Direction {
+    case north, south, east, west
+}
 
+protocol SteerAbility {
+    var direction: Direction {get set}
+    mutating func steerInDirection(_ direction: Direction)
+}
 
+extension SteerAbility {
+    mutating func steerInDirection(_ direction: Direction) {
+        self.direction = direction
+    }
+}
+
+class SailBoat: SteerAbility {
+    var direction: Direction = .north
+}
+
+class RowBoat: SteerAbility {
+    var direction: Direction = .north
+}
+
+class SteamBoat: SteerAbility {
+    var direction: Direction = .north
+}
+/*
+(1) - In the Ships.swift file, create an enum called Direction that has four cases. north, south, east and west.
+
+(2) - Below the Direction enum, create a new protocol called SteerAbility. This protocol should require any class/struct conforming to it to provide a property called direction of type Direction that is both gettable and settable. 
+ 
+    It should also require that the class/struct conforming to it should provide a function called steerInDirection(_:). This function should take in an argument called direction which is of type Direction. 
+ 
+    Mark this function as mutating, because in its implementation (which will we will do soon), it will change the value of one of its properties and because it's doing that we need to mark it as mutating. This only affects structs and not classes, but we don't know who will be implementing this protocol, it could be both structs & classes so we are required to mark it as mutating no matter what.
+
+(3) - Below this newly made Protocol - create an extension on the SteerAbility protocol.
+
+Like this:
+extension SteerAbility {
+    
+}
+We've just extended a protocol.
+
+ In that extension (within the curly braces). Create a mutating function (the EXACT one you're asking classes or structs to implement if they were to conform to the SteerAbility protocol.) This function should be called steerInDirection(_:). It should have one argument called direction of type Direction. It should also be marked as a mutating. You're going to now implement this function. In its implementation, you should set the direction property to equal the direction argument to this function.
+ self.direction = direction
+ The reason you have to write self.direction is because the argument label matches the name of the instance property.
+ 
+ (4) - Still in the Ships.swift file, below where you just created the extension on SteerAbility, create a new class called SailBoat which conforms to the SteerAbility protocol. In conforming to this protocol, you might think that you need to provide both the direction instance property and the steerInDirection(_:) function but you're only required to provide the direction property because the steerInDirection(_:) has a default implementation. The implementation of the steerInDirection(_:) function you provided above within the extension has given us this default implementation.
+ So provide only an instance property called direction of type Direction with a default value of .north. Since all properties (only one in this case) are set to an actual value, you aren't required to provide an initializer.
+ (5) - Below the SailBoat class, create a class called RowBoat which also conforms to the SteerAbility protocol. Just like in the above instruction, only provide a direction property with a default value of .north.
+ (6) - Create a class called SteamBoat that also conforms to the SteerAbility protocol. Just like above, you should only provide a direction property with a default value of .north. You should not implement the steerInDirection(_:) as this already has a default implementation.
+*/
 
 
 
